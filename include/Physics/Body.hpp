d49bed9c6f67e647f7653140dbe5eb749308dbbe
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-04 13:41:51 +0300

    added override

--- include/Physics/Body.hpp
+++ include/Physics/Body.hpp
@@ -18,6 +18,9 @@ namespace AnimeDefendersEngine {
             BodyDefinition bodyDefinition;
 
          public:
+            virtual auto applyForce(Math::Vector2<float> vec) -> void override;
+            virtual auto applyImpulse(Math::Vector2<float> vec) -> void override;
+            virtual auto getPosition() -> Math::Vector2<float> override;
             auto setType(BodyType type) -> void;
         };
     }  // namespace PhysicsEngine
