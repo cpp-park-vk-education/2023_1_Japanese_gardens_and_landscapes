71526360c6540faac08c3ec09e25f3a25576149b
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-04 12:41:54 +0300

    Added shape.cpp

---
+++ src/Physics/Shape.cpp
@@ -0,0 +1,11 @@
+#include "Shape.hpp"
+
+using namespace AnimeDefendersEngine::PhysiscEngine;
+
+Shape::~Shape(){};
+
+Shape::getType() const {};
+
+Circle::Circle(float radius){};
+
+Rectangle::Ractangle(Math::Vector2 size){};
