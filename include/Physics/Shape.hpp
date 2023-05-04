5a983b2b61ec78bbe1e93a1033e11cc93066f029
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-04 08:44:46 +0300

    Added Shape.hpp

---
+++ include/Physics/Shape.hpp
@@ -0,0 +1,29 @@
+namespace AnimeDefendersEngine {
+    namespace PhysicsEngine {
+
+        class Shape {
+         public:
+            enum class Type {
+                circle = 0,
+                rectangle
+            };
+            virtual ~Shape(){};
+            Type getType() const {};
+
+         protected:
+            Type m_type;
+        };
+
+        class Circle : public Shape {
+         public:
+            Circle(float radius) : radius(radius) { m_type = Type::circle; };
+            float radius;
+        };
+
+        class Rectangle : public Shape {
+         public:
+            // Rectangle(vector2 size) : size(size) { m_type = Type::rectangle; };
+            // vector2 size;
+        };
+    }  // namespace PhysicsEngine
+}  // namespace AnimeDefendersEngine
