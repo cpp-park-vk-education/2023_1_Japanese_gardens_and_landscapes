441631bbae7dd02ee3ba46f21d839cebfc78b591
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-04 11:12:02 +0300

    add systemUpdate

--- include/Physics/PhysicsSystem.hpp
+++ include/Physics/PhysicsSystem.hpp
@@ -6,7 +6,8 @@ namespace AnimeDefendersEngine {
     namespace PhysicsEngine {
         class PhysicsSystem : public ISystem {
          public:
-            virtual auto updateSystem(ComponentManager&) -> void override{};
+            virtual auto updateSystem(ComponentManager& componentManager) -> void override{};
+            virtual auto updateSystem(ComponentManager& componentManager, float timeStep) -> void override{};
 
          private:
             PhysicsWorld physicsWorld;
