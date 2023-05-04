b24180e261787f8c7877bb9fe7bb7d734d1a9b8f
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-04 11:27:20 +0300

    Fixes

--- include/AnimeFramework/Component.hpp
+++ include/AnimeFramework/Component.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace AnimeDefendersEngine {
-
+enum class ComponentType{};
     struct Component {};
 
 }  // namespace AnimgeDefendersEngine
---
+++ src/AnimeFramework/EventManager.cpp
@@ -0,0 +1,5 @@
+#include "EventManager.hpp"
+
+using namespace AnimeDefendersEngine;
+
+auto EventManager::hasEvent(std::string eventName) -> bool {};
