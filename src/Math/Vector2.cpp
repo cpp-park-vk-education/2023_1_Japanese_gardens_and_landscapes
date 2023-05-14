3c5d2e614b9ebb132bf78069312de1854b9c036c
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-14 23:20:38 +0300

    added <T> to Vector2 to fix error

--- src/Math/Vector2.cpp
+++ src/Math/Vector2.cpp
@@ -3,7 +3,7 @@
 using namespace AnimeDefendersEngine::Math;
 
 template <typename T>
-Vector2::Vector2(T val1 = T{}, T val2 = T{}){};
+Vector2<T>::Vector2(T val1 = T{}, T val2 = T{}){};
 
 template <typename T>
-auto Vector2::operator[](std::size_t i) -> T& {};
+auto Vector2<T>::operator[](std::size_t i) -> T& {};
