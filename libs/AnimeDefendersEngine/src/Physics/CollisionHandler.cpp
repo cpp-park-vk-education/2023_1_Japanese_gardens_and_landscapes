135358a7d84aeedc49a2f05dc13258455abb4a0a
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-16 22:55:23 +0300

    added broadPhase and narrowPhase

--- libs/AnimeDefendersEngine/src/Physics/CollisionHandler.cpp
+++ libs/AnimeDefendersEngine/src/Physics/CollisionHandler.cpp
@@ -1,17 +1,41 @@
 #include "CollisionHandler.hpp"
+#include "Body.hpp"
+#include "Vector2.hpp"
 
 using namespace AnimeDefendersEngine::Physics;
 
-auto CollisionHandler::broadPhase(const std::vector<IBody*>&) -> std::vector<Manifold> {
-    return {};
+auto CollisionHandler::broadPhase(const std::vector<IBody*>& bodies) -> std::vector<Manifold> {
+    std::vector<Manifold> contacts(bodies.size());
+    for (size_t i = 0; i < bodies.size(); ++i) {
+        IBody* bodyA = bodies.at(i);
+        for (size_t j = i + 1; j < bodies.size(); ++j) {
+            IBody* bodyB = bodies.at(i);
+            Manifold contact(bodyA, bodyB);
+            contacts.push_back(contact);
+        }
+    }
+    return contacts;
 }
 
-auto CollisionHandler::narrowPhase(std::vector<Manifold>&) -> void {}
+auto CollisionHandler::narrowPhase(std::vector<Manifold>& contacts) -> void {
+    for (auto contactIterator = contacts.begin(); contactIterator != contacts.end();) {
+        if (hasCollision(contactIterator->bodyA, contactIterator->bodyB)) {
+            if (contactIterator->bodyA->isTrigger() || contactIterator->bodyB->isTrigger()) {
+                continue;
+            }
+            specifyCollision(*contactIterator);
+            resolveCollision(*contactIterator);
+            contactIterator++;
+        } else {
+            contacts.erase(contactIterator);
+        }
+    }
+};
 
-auto CollisionHandler::hasCollision(IBody*, IBody*) -> bool {
-    return false;
-}
+auto CollisionHandler::specifyCollision(Manifold& contact) -> void{
 
-auto CollisionHandler::specifyCollision(Manifold) -> void {}
+};
 
-auto CollisionHandler::resolveCollision(Manifold) -> void {}
+auto CollisionHandler::resolveCollision(Manifold& contact) -> void{
+
+};
