78d94ae851696475ec5ce1a5982bd9b4e96c17ac
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-16 22:56:27 +0300

    added hasCollision functions

--- libs/AnimeDefendersEngine/src/Physics/CollisionHandler.cpp
+++ libs/AnimeDefendersEngine/src/Physics/CollisionHandler.cpp
@@ -32,6 +32,54 @@ auto CollisionHandler::narrowPhase(std::vector<Manifold>& contacts) -> void {
     }
 };
 
+static auto hasCollisionCircleCircle(IBody* bodyA, IBody* bodyB) -> bool {
+    Circle* circleA = dynamic_cast<Circle*>(bodyA->getShape());
+    Circle* circleB = dynamic_cast<Circle*>(bodyB->getShape());
+    float distance = (bodyA->getPosition() - bodyB->getPosition()).norm();
+    return distance < circleA->radius + circleB->radius;
+}
+
+static auto hasCollisionRectangleCircle(IBody* bodyA, IBody* bodyB) -> bool {
+    Rectangle* rectangleA = dynamic_cast<Rectangle*>(bodyA->getShape());
+    Circle* circleB = dynamic_cast<Circle*>(bodyB->getShape());
+    AnimeDefendersEngine::Math::Vector2<float> distance = bodyB->getPosition() - bodyA->getPosition();
+    AnimeDefendersEngine::Math::Vector2<float> closestVertexToCircleCenter = distance;
+    float x_extent = rectangleA->size.x / 2;
+    float y_extent = rectangleA->size.y / 2;
+    closestVertexToCircleCenter.x = std::min(closestVertexToCircleCenter.x, x_extent);
+    closestVertexToCircleCenter.x = std::max(closestVertexToCircleCenter.x, -x_extent);
+    closestVertexToCircleCenter.y = std::min(closestVertexToCircleCenter.y, y_extent);
+    closestVertexToCircleCenter.y = std::max(closestVertexToCircleCenter.y, -y_extent);
+
+    AnimeDefendersEngine::Math::Vector2<float> normal = distance - closestVertexToCircleCenter;
+
+    return normal.norm() < circleB->radius;
+};
+
+static auto hasCollisionCircleRectangle(IBody* bodyA, IBody* bodyB) -> bool {
+    return hasCollisionRectangleCircle(bodyB, bodyA);
+};
+
+static auto hasCollisionRectangleRectangle(IBody* bodyA, IBody* bodyB) -> bool {
+    Rectangle* rectangleA = dynamic_cast<Rectangle*>(bodyA->getShape());
+    Rectangle* rectangleB = dynamic_cast<Rectangle*>(bodyB->getShape());
+
+    AnimeDefendersEngine::Math::Vector2<float> normal = bodyB->getPosition() - bodyA->getPosition();
+    float xOverlap = (rectangleA->size.x + rectangleB->size.x) / 2 - normal.norm();
+    return xOverlap > 0;
+};
+
+using hasCollisionFunctionPtr = bool (*)(IBody* bodyA, IBody* bodyB);
+
+static hasCollisionFunctionPtr hasCollisionTypes[Shape::shapeCount][Shape::shapeCount] = {
+    {hasCollisionCircleCircle,    hasCollisionCircleRectangle   },
+    {hasCollisionRectangleCircle, hasCollisionRectangleRectangle}
+};
+
+auto CollisionHandler::hasCollision(IBody* bodyA, IBody* bodyB) -> bool {
+    return hasCollisionTypes[bodyA->getShapeType()][bodyB->getShapeType()](bodyA, bodyB);
+};
+
 auto CollisionHandler::specifyCollision(Manifold& contact) -> void{
 
 };
