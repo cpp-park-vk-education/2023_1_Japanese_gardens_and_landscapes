2f385d214fef42817e080d7a9871295329200976
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-16 16:10:51 +0300

    Мусещк2 ашс

--- libs/AnimeDefendersEngine/include/Math/Vector2.hpp
+++ libs/AnimeDefendersEngine/include/Math/Vector2.hpp
@@ -7,10 +7,10 @@ namespace AnimeDefendersEngine {
         struct Vector2 {
             explicit Vector2(T val1 = T{}, T val2 = T{});
             auto norm() -> T&;
-            auto operator+(const Vector2& vec1, const Vector2& vec2) -> Vector2&;
-            auto operator-(const Vector2& vec1, const Vector2& vec2) -> Vector2&;
-            auto operator*(const Vector2& vec1, const Vector2& vec2) -> T&;
-            auto operator*(const Vector2& vec, const T& const1) -> Vector2&;
+            auto operator+(const Vector2& vec1) -> Vector2&;
+            auto operator-(const Vector2& vec1) -> Vector2&;
+            auto operator*(const Vector2& vec1) -> T&;
+            auto operator*(const T& const1) -> Vector2&;
             T x;
             T y;
         };
