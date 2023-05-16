7186badc2fc7029430f204565d3ebe53fa029b10
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-16 16:06:43 +0300

    Added Vector2.cpp

---
+++ libs/AnimeDefendersEngine/src/Math/Vector2.cpp
@@ -0,0 +1,24 @@
+#include "Vector2.hpp"
+
+using namespace AnimeDefendersEngine::Math;
+
+auto Vector2::operator+(const Vector2& vec1, const Vector2& vec2) -> Vector2&
+{
+    Vector2 newVec{};
+    newVec.x = vec1.x + vec2.x;
+    newVec.y = vec1.y + vec2.y;
+    return newVec;
+};
+auto Vector2::operator-(const Vector2& vec1, const Vector2& vec2) -> Vector2& {
+    Vector2 newVec{};
+    newVec.x = vec1.x - vec2.x;
+    newVec.y = vec1.y - vec2.y;
+    return newVec;
+};
+auto Vector2::operator*(const Vector2& vec1, const Vector2& vec2) -> T& {
+    T scalar{};
+    scalar = vec1.x * vec1.x + vec2.y * vec2.y;
+    return scalar;
+};
+auto Vector2::operator*(const Vector2& vec, const T& const1) -> Vector2& {
+};
