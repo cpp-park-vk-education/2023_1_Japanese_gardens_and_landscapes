8cd38c5050e3bfde9bcdb826f01084379bfe9a62
AngelicHedgehog <serj_1313@mail.ru>
2023-05-16 20:49:31 +0300

    Base implementation of Renderer

--- libs/AnimeDefendersEngine/src/Graphics/Renderer.cpp
+++ libs/AnimeDefendersEngine/src/Graphics/Renderer.cpp
@@ -1,7 +1,35 @@
 #include "Renderer.hpp"
+#include "UiElement.hpp"
+
+#include <cassert>
+#include <ranges>
 
 using namespace AnimeDefendersEngine::Graphics;
 
-Renderer::Renderer(std::shared_ptr<IWindow>) {}
+Renderer::Renderer(std::shared_ptr<IWindow> window) : m_window(window) {
+    m_window->createWindow();
+}
 
-auto Renderer::renderObjects(const Scene&) -> void {}
+auto Renderer::renderObjects(const Scene& scene) -> void {
+    auto componentManager = scene.getComponentManager();
+
+    auto cameras = componentManager->getComponents(ComponentType::Camera);
+    auto sprites = componentManager->getComponents(ComponentType::Sprite);
+    auto uiElements = componentManager->getComponents(ComponentType::UiElement);
+
+    std::vector<Component*> activeCameras =
+        std::as_const(cameras) | std::ranges::views::filter([](const auto& camera) { return camera->isCameraActive(); });
+
+    assert(activeCameras.size() == 1);
+
+    auto activeCamera = activeCameras.at(0);
+    activeCamera->determineNewCameraTransform();
+    activeCamera->applyCameraView();
+
+    std::as_const(sprites) | std::ranges::views::filter([](const auto& sprite) { return sprite->isSpriteVisibleToCamera(activeCamera); }) |
+        std::ranges::views::transform([](const auto& sprite) { return sprite->drawSprite(); });
+
+    std::as_const(uiElements) | std::ranges::views::transform([](const auto& uiElement) { return uiElement->drawUiElement(); });
+
+    m_window->updateFrame();
+}
