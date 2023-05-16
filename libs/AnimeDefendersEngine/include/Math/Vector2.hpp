303faaa02e260c62e07c21545cff217f549774df
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-16 16:30:12 +0300

    Vector2

--- libs/AnimeDefendersEngine/include/Math/Vector2.hpp
+++ libs/AnimeDefendersEngine/include/Math/Vector2.hpp
@@ -7,12 +7,13 @@ namespace AnimeDefendersEngine {
         struct Vector2 {
             explicit Vector2(T val1 = T{}, T val2 = T{});
             auto norm() -> T&;
-            auto operator+(const Vector2& vec1) -> Vector2&;
-            auto operator-(const Vector2& vec1) -> Vector2&;
-            auto operator*(const Vector2& vec1) -> T&;
-            auto operator*(const T& const1) -> Vector2&;
             T x;
             T y;
+
+            friend auto operator+(const Vector2& vec1, const Vector2& vec2) -> Vector2&;
+            friend auto operator-(const Vector2& vec1, const Vector2& vec2) -> Vector2&;
+            friend auto operator*(const Vector2& vec1, const Vector2& vec2) -> T&;
+            friend auto operator*(const T& const1, const Vector2& vec1) -> Vector2&;
         };
 
     }  // namespace Math
--- libs/AnimeDefendersEngine/src/Math/Vector2.cpp
+++ libs/AnimeDefendersEngine/src/Math/Vector2.cpp
@@ -2,21 +2,11 @@
 
 using namespace AnimeDefendersEngine::Math;
 
-auto Vector2::operator+(const Vector2& vec1) -> Vector2& {
-    Vector2 newVec{};
-    newVec.x = vec1.x + vec2.x;
-    newVec.y = vec1.y + vec2.y;
-    return newVec;
-};
-auto Vector2::operator-(const Vector2& vec1) -> Vector2& {
-    Vector2 newVec{};
-    newVec.x = vec1.x - vec2.x;
-    newVec.y = vec1.y - vec2.y;
-    return newVec;
-};
-auto Vector2::operator*(const Vector2& vec1) -> T& {
-    T scalar{};
-    scalar = vec1.x * vec1.x + vec2.y * vec2.y;
-    return scalar;
-};
-auto Vector2::operator*(const T& const1) -> Vector2& {};
+template <typename T>
+auto Vector2<T>::operator+(const Vector2& vec1, const Vector2& vec2) -> Vector2& {};
+template <typename T>
+auto Vector2<T>::operator-(const Vector2& vec1, const Vector2& vec2) -> Vector2& {};
+template <typename T>
+auto Vector2<T>::operator*(const Vector2& vec1, const Vector2& vec2) -> T& {};
+template <typename T>
+auto Vector2<T>::operator*(const T& const1, const Vector2& vec1) -> Vector2& {};
