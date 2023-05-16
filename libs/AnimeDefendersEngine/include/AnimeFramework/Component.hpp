e78ad5839815aba119deb4311a729b7a4ebea50d
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-16 12:09:38 +0300

    Enchancment: realized Component registration in ComponentManager

--- libs/AnimeDefendersEngine/include/AnimeFramework/Component.hpp
+++ libs/AnimeDefendersEngine/include/AnimeFramework/Component.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "ComponentManager.hpp"
+
+#include <typeindex>
+
 namespace AnimeDefendersEngine {
 
+    /*
     enum class ComponentType {
         Sprite,
         Camera,
@@ -10,7 +15,28 @@ namespace AnimeDefendersEngine {
         Collider2DComponent,
         RigidBody2DComponent
     };
+    */
 
-    struct Component {};
+    class Component {
+     public:
+        Component(std::type_index typeId, ComponentManager& compManager) : m_typeId{typeId}, m_compManager{compManager} {
+            m_compManager.addComponent(m_typeId, this);
+        }
+
+        virtual ~Component() {
+            if (is_ComponentManagerExists) {
+                m_compManager.deleteComponent(m_typeId, this);
+            }
+        }
+
+     private:
+        friend class ComponentManager;
+        void m_componentManagerDestroyed() { is_ComponentManagerExists = false; }
+
+     private:
+        bool is_ComponentManagerExists{true};
+        std::type_index m_typeId;
+        ComponentManager& m_compManager;
+    };
 
 }  // namespace AnimeDefendersEngine
--- libs/AnimeDefendersEngine/include/AnimeFramework/ComponentManager.hpp
+++ libs/AnimeDefendersEngine/include/AnimeFramework/ComponentManager.hpp
@@ -1,25 +1,22 @@
 #pragma once
 
-#include "Component.hpp"
 #include "MemoryMaster.hpp"
 
+#include <typeindex>
 #include <unordered_map>
 #include <vector>
 
 namespace AnimeDefendersEngine {
+    class Component;
 
-    class IComponentManager {
+    class ComponentManager {
      public:
-        virtual void addComponent(ComponentType typeId, Component* component) = 0;
-        virtual auto getComponents(ComponentType typeId) -> std::vector<Component*> = 0;
-    };
-
-    class ComponentManager : public IComponentManager {
-     public:
-        void addComponent(ComponentType typeId, Component* component) override;
-        auto getComponents(ComponentType typeId) -> std::vector<Component*> override;
+        ~ComponentManager();
+        void addComponent(std::type_index typeId, Component* component);
+        void deleteComponent(std::type_index typeId, Component* compoment);
+        auto getComponents(std::type_index typeId) -> std::vector<Component*>;
 
      private:
-        std::unordered_map<ComponentType, std::vector<Component*>> m_components;
+        std::unordered_map<std::type_index, std::vector<Component*>> m_components;
     };
 }  // namespace AnimeDefendersEngine
