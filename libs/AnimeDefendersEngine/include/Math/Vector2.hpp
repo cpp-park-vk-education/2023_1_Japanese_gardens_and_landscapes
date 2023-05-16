fa2a45a4e0acc971fb5269333ca927fb0fb15c7c
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-16 15:56:48 +0300

    Added Vector2 declarations and added to cmake

--- libs/AnimeDefendersEngine/include/Math/Vector2.hpp
+++ libs/AnimeDefendersEngine/include/Math/Vector2.hpp
@@ -1,18 +1,17 @@
 #pragma once
 
-#include <vector>
-
 namespace AnimeDefendersEngine {
     namespace Math {
 
         template <typename T>
-        class Vector2 {
-         public:
-            explicit Vector2(T val1 = T{}, T val2 = T{}) { std::swap(val1, val2); }
-            auto operator[](std::size_t i) -> T& {}
-
-         private:
-            std::vector<T> m_values;
+        struct Vector2 {
+            explicit Vector2(T val1 = T{}, T val2 = T{});
+            auto operator+(const Vector2& vec1, const Vector2& vec2) -> Vector2&;
+            auto operator-(const Vector2& vec1, const Vector2& vec2) -> Vector2&;
+            auto operator*(const Vector2& vec1, const Vector2& vec2) -> T&;
+            auto operator*(const Vector2& vec, const T& const1) -> Vector2&;
+            T x;
+            T y;
         };
 
     }  // namespace Math
