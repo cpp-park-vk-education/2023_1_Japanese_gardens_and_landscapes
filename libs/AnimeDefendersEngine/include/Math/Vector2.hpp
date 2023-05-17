78c52c79c496ba7fda6e98fa51f25cdf8838e8bc
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-17 22:14:23 +0300

    Added Vector2 implementation

--- libs/AnimeDefendersEngine/include/Math/Vector2.hpp
+++ libs/AnimeDefendersEngine/include/Math/Vector2.hpp
@@ -1,33 +1,44 @@
 #pragma once
 
+#include <iostream>
+#include "math.h"
+
 namespace AnimeDefendersEngine {
     namespace Math {
-        template <typename T>
-        struct Vector2;
-        template <typename T>
-        auto operator+(const Vector2<T>& vec1, const Vector2<T>& vec2) -> Vector2<T>{};
-        template <typename T>
-        auto operator-(const Vector2<T>& vec1, const Vector2<T>& vec2) -> Vector2<T>{};
-        template <typename T>
-        auto operator*(const Vector2<T>& vec1, const Vector2<T>& vec2) -> T{};
-        template <typename T>
-        auto operator*(const T& const1, const Vector2<T>& vec1) -> Vector2<T>{};
 
         template <typename T>
         struct Vector2 {
-            explicit Vector2(T val1 = T{}, T val2 = T{});
-            auto norm() -> T&;
+            explicit Vector2(T val1 = T{}, T val2 = T{}) : x(val1), y(val2) {}
             T x;
             T y;
 
-            friend auto operator+<T>(const Vector2<T>& vec1, const Vector2<T>& vec2) -> Vector2<T>;
-
-            friend auto operator-<T>(const Vector2<T>& vec1, const Vector2<T>& vec2) -> Vector2<T>;
-
-            friend auto operator*<T>(const Vector2<T>& vec1, const Vector2<T>& vec2) -> T;
-
-            friend auto operator*<T>(const T& const1, const Vector2<T>& vec1) -> Vector2<T>;
+            auto norm() -> T { return sqrt(this * this); }
+            auto print() -> void { std::cout << "(" << x << ", " << y << ")" << std::endl; }
         };
+
+        template <typename T>
+        auto operator+(const Vector2<T>& vec1, const Vector2<T>& vec2) -> Vector2<T> {
+            return Vector2<T>(vec1.x + vec2.x, vec1.y + vec2.y);
+        }
+        template <typename T>
+        auto operator-(const Vector2<T>& vec1, const Vector2<T>& vec2) -> Vector2<T> {
+            return Vector2<T>(vec1.x - vec2.x, vec1.y - vec2.y);
+        }
+
+        template <typename T>
+        auto operator*(const Vector2<T>& vec1, const Vector2<T>& vec2) -> T {
+            return vec1.x * vec2.x + vec1.y * vec2.y;
+        }
+
+        template <typename T>
+        auto operator*(const double& c1, const Vector2<T>& vec1) -> Vector2<T> {
+            return Vector2<T>(c1 * vec1.x, c1 * vec1.y);
+        }
+
+        template <typename T>
+        auto operator*(const Vector2<T>& vec1, const double& c1) -> Vector2<T> {
+            return Vector2<T>(c1 * vec1.x, c1 * vec1.y);
+        }
 
     }  // namespace Math
 }  // namespace AnimeDefendersEngine
