8e499916e170f712ebf4566352bbdc9fe48e8c7d
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-17 13:50:38 +0300

    replaced Ibody with body

--- libs/AnimeDefendersEngine/include/Physics/CollisionHandler.hpp
+++ libs/AnimeDefendersEngine/include/Physics/CollisionHandler.hpp
@@ -10,22 +10,22 @@ namespace AnimeDefendersEngine {
 
         class ICollisionHandler {
          public:
-            virtual auto broadPhase(const std::vector<IBody*>& bodies) -> std::vector<Manifold> = 0;
+            virtual auto broadPhase(const std::vector<Body*>& bodies) -> std::vector<Manifold> = 0;
             virtual auto narrowPhase(std::vector<Manifold>& contacts) -> void = 0;
 
          protected:
-            virtual auto hasCollision(IBody* bodyA, IBody* bodyB) -> bool = 0;
+            virtual auto hasCollision(Body* bodyA, Body* bodyB) -> bool = 0;
             virtual auto specifyCollision(Manifold& contact) -> void = 0;
             virtual auto resolveCollision(Manifold& contact) -> void = 0;
         };
 
         class CollisionHandler : public ICollisionHandler {
          public:
-            virtual auto broadPhase(const std::vector<IBody*>& bodies) -> std::vector<Manifold> override;
+            virtual auto broadPhase(const std::vector<Body*>& bodies) -> std::vector<Manifold> override;
             virtual auto narrowPhase(std::vector<Manifold>& bodies) -> void override;
 
          private:
-            virtual auto hasCollision(IBody* bodyA, IBody* bodyB) -> bool override;
+            virtual auto hasCollision(Body* bodyA, Body* bodyB) -> bool override;
             virtual auto specifyCollision(Manifold& contact) -> void override;
             virtual auto resolveCollision(Manifold& contact) -> void override;
         };
