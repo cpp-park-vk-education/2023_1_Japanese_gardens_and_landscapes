054d47bfed9de4b93480fedba974e4e5122fbbaa
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-17 19:00:40 +0300

    added default constructor for BodyDefinition

--- libs/AnimeDefendersEngine/include/Physics/BodyDefinition.hpp
+++ libs/AnimeDefendersEngine/include/Physics/BodyDefinition.hpp
@@ -30,6 +30,7 @@ namespace AnimeDefendersEngine {
             BodyDefinition(size_t id, std::unique_ptr<Shape>&& shapeUPtr, BodyType bodyType, Transform transform,
                            Math::Vector2<float> velocity, size_t layers, Math::Vector2<float> force, bool isTrigger);
             BodyDefinition(BodyDefinition&& other);
+            BodyDefinition();
         };
     }  // namespace Physics
 }  // namespace AnimeDefendersEngine
