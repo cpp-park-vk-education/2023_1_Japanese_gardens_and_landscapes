8975a70b43eacf348591d217f1398469e4e9c30b
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-17 21:59:08 +0300

    added copy constructor

--- libs/AnimeDefendersEngine/include/Physics/BodyDefinition.hpp
+++ libs/AnimeDefendersEngine/include/Physics/BodyDefinition.hpp
@@ -19,18 +19,22 @@ namespace AnimeDefendersEngine {
         };
 
         struct BodyDefinition {
-            size_t id;
-            std::unique_ptr<Shape> shapeUPtr;
-            BodyType bodyType;
-            Transform transform;
-            Math::Vector2<float> velocity;
-            size_t layers;
-            Math::Vector2<float> force;
-            bool isTrigger;
+            size_t id{};
+            std::unique_ptr<Shape> shapeUPtr{nullptr};
+            BodyType bodyType{};
+            Transform transform{};
+            Math::Vector2<float> velocity{};
+            size_t layers{};
+            Math::Vector2<float> force{};
+            bool isTrigger{};
             BodyDefinition(size_t id, std::unique_ptr<Shape>&& shapeUPtr, BodyType bodyType, Transform transform,
                            Math::Vector2<float> velocity, size_t layers, Math::Vector2<float> force, bool isTrigger);
             BodyDefinition(BodyDefinition&& other);
-            BodyDefinition();
+            BodyDefinition(){};
+            BodyDefinition(const BodyDefinition& other) = delete;
+            BodyDefinition& operator=(const BodyDefinition& other) = delete;
+            BodyDefinition& operator=(BodyDefinition&& other) = delete;
+            ~BodyDefinition(){};
         };
 
     }  // namespace Physics
