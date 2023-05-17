f4d567924b481dddfca42811b24adabc17cab722
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-17 14:20:52 +0300

    Fix: CI

--- tests/AnimeFramework.cpp
+++ tests/AnimeFramework.cpp
@@ -1,10 +1,6 @@
-#include "Logger.hpp"
-
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 TEST(SampleTest, TestSoThisFileWontBeEmpty) {
-    using namespace AnimeDefendersEngine::Logger;
-    deafult_log.setLogLevel(LogLevel::NoLog);
     ASSERT_TRUE(true);
 }
