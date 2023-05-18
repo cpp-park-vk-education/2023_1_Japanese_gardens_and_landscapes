e91a8b3033c8ed6c4feca63e6e4d8b3143f2c877
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-18 11:03:40 +0300

    added deltaTime to argument of SystemManager

--- libs/AnimeDefendersEngine/include/Systems/SystemManager.hpp
+++ libs/AnimeDefendersEngine/include/Systems/SystemManager.hpp
@@ -6,12 +6,12 @@ namespace AnimeDefendersEngine {
 
     class ISystemManager {
      public:
-        virtual void updateSystems(ComponentManager&) = 0;
+        virtual void updateSystems(ComponentManager&, float) = 0;
     };
 
     class SystemManager : public ISystemManager {
      public:
-        void updateSystems(ComponentManager&) override;
+        void updateSystems(ComponentManager&, float) override;
 
      private:
         std::vector<ISystem*> m_systems;
