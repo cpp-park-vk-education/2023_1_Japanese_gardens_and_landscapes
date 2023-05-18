4d59a01ac58d5a5a20efb127c9bf95227420c77c
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-18 10:14:32 +0300

    added fields to GameLoop

--- libs/AnimeDefendersEngine/include/AnimeFramework/GameLoop.hpp
+++ libs/AnimeDefendersEngine/include/AnimeFramework/GameLoop.hpp
@@ -2,6 +2,8 @@
 
 // static_assert(false, "Need to add propper headers for IEventManager and InputManager");
 #include "InputManager.hpp"
+#include "Renderer.hpp"
+#include "SceneManager.hpp"
 #include "SystemManager.hpp"
 
 #include <memory>
@@ -15,10 +17,17 @@ namespace AnimeDefendersEngine {
         void setEventManager(std::unique_ptr<IEventManager>);
 
      private:
-        bool is_running{true};
+        bool m_isRunning{true};
+        float m_maxDeltaTime{1.0f / 25.0f};
+        float m_fixedDeltaTime{1.0f / 60.0f};
+
         std::unique_ptr<ISystemManager> m_systemManager;
         std::unique_ptr<IEventManager> m_eventManager;
-        std::unique_ptr<InputManager> m_input;
+        std::unique_ptr<InputManager> m_inputManager;
+        std::unique_ptr<Graphics::Renderer> m_renderer;
+
+        SceneManager* m_sceneManager;
+        ComponentManager* m_componentManager;
     };
 
 }  // namespace AnimeDefendersEngine
