0d629da27d4efc808479dd75923f44595463478f
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-19 19:36:00 +0300

    Separated definition and realization

--- libs/AnimeDefendersEngine/include/AnimeFramework/Component.hpp
+++ libs/AnimeDefendersEngine/include/AnimeFramework/Component.hpp
@@ -7,9 +7,9 @@ namespace AnimeDefendersEngine {
 
     class Component {
      public:
-        explicit Component(const std::string& entityId) : m_entityId{entityId} {}
+        explicit Component(const std::string& entityId);
 
-        [[nodiscard]] auto getEntityId() const noexcept -> std::string { return m_entityId; }
+        [[nodiscard]] auto getEntityId() const noexcept -> std::string;
 
      private:
         std::string m_entityId;
