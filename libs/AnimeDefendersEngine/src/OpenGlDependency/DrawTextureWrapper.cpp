cd633da7e51ae26daf43fbf364452bc1e49807fe
AngelicHedgehog <serj_1313@mail.ru>
2023-05-20 20:42:52 +0300

    Changed cbegin to begin back

--- libs/AnimeDefendersEngine/src/OpenGlDependency/DrawTextureWrapper.cpp
+++ libs/AnimeDefendersEngine/src/OpenGlDependency/DrawTextureWrapper.cpp
@@ -14,7 +14,7 @@ auto DrawTextureWrapper::loadTexture(const Math::Matrix2d<Color>& imageData) con
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageData.columns(), imageData.rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, &*imageData.cbegin());
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageData.columns(), imageData.rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, &*imageData.begin());
 
     glBindTexture(GL_TEXTURE_2D, 0);
 
