12d28d4ac37a1b0561958894e575bdbb5c85fe1f
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-20 12:33:33 +0300

    updated physics update

--- libs/AnimeDefendersEngine/src/Physics/PhysicsSystem.cpp
+++ libs/AnimeDefendersEngine/src/Physics/PhysicsSystem.cpp
@@ -1,45 +1,53 @@
 #include "PhysicsSystem.hpp"
-// #include "BaseComponent.hpp"
 #include <memory>
+#include "BaseComponent.hpp"
 #include "Body.hpp"
 #include "BodyDefinition.hpp"
 #include "Component.hpp"
 #include "ComponentManager.hpp"
 using namespace AnimeDefendersEngine::Physics;
 
-// class PhysicsComponent : public AnimeDefendersEngine::BaseComponent<PhysicsComponent> {
-//  public:
-//     PhysicsComponent(AnimeDefendersEngine::ComponentManager& compManager)
-//         : AnimeDefendersEngine::BaseComponent<PhysicsComponent>(compManager) {
+class RigidBody2DComponent : public AnimeDefendersEngine::BaseComponent<RigidBody2DComponent> {
+ public:
+    RigidBody2DComponent(AnimeDefendersEngine::ComponentManager& compManager)
+        : AnimeDefendersEngine::BaseComponent<RigidBody2DComponent>(compManager) {}
 
-//     }
-//     bool isStatic{false};
+    AnimeDefendersEngine::Math::Vector2f velocity;
+};
 
-// };
+class Collider2DComponent : public AnimeDefendersEngine::BaseComponent<Collider2DComponent> {
+ public:
+    Collider2DComponent(AnimeDefendersEngine::ComponentManager& compManager)
+        : AnimeDefendersEngine::BaseComponent<Collider2DComponent>(compManager) {}
+
+    AnimeDefendersEngine::Math::Vector2f size;
+    RigidBody2DComponent* rigidBody2DComponent;
+    TransformComponent* transformComponent;
+};
 
 // У коллайдера ссылка на трансформ и риджид бади
 
 auto PhysicsSystem::updateSystem(SceneManager& sceneManager, float fixedDeltaTime) -> void {
     using AnimeDefendersEngine::Math::Vector2f;
 
-    ComponentManager& componentManager = sceneManager.getActiveScene()->getComponentManager();
-    auto components = componentManager.getComponents<Collider2DComponent>();
+    ComponentManager& componentManager = sceneManager.getActiveScene().getComponentManager();
+    auto colliders = componentManager.getComponents<Collider2DComponent>();
 
     std::vector<Body*> bodies;
-    bodies.reserve(components.size());
-    for (auto component : components) {
+    bodies.reserve(colliders.size());
+    for (auto collider : colliders) {
         BodyDefinition bodyDef;
-        bodyDef.id = component.getEntityID();
-        bodyDef.shape = std::make_unique<Rectangle>(Collider2DComponent.size);
-        bodyDef.transform.position = component.TransformComponent.position;
+        bodyDef.id = collider.getEntityID();
+        bodyDef.shape = std::make_unique<Rectangle>(collider.size);
+        bodyDef.transform.position = collider.transformComponent.position;
 
         Body* body = m_physicsWorld.addBody(std::move(bodyDef));
 
-        if (component.RigidBody2DComponent == nullptr) {
+        if (collider.rigidBody2DComponent == nullptr) {
             body->setType(BodyType::staticBody);
         } else {
             body->setType(BodyType::dynamicBody);
-            body->applyImpulse(component.RigidBody2DComponent.velocity);
+            body->applyImpulse(collider.rigidBody2DComponent.velocity);
         }
 
         bodies.push_back(body);
@@ -49,7 +57,7 @@ auto PhysicsSystem::updateSystem(SceneManager& sceneManager, float fixedDeltaTim
     m_physicsWorld.fixedUpdate();
 
     for (auto body : bodies) {
-        components.at(body->getID()).TransformComponent.position = body->getPosition();
+        colliders.at(body->getID()).transformComponent.position = body->getPosition();
     }
 }
 
