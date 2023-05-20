baef362ddb2fae6c046744d483168a59da3ac346
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-20 11:29:05 +0300

    fixed updateSystem

--- libs/AnimeDefendersEngine/src/Physics/PhysicsSystem.cpp
+++ libs/AnimeDefendersEngine/src/Physics/PhysicsSystem.cpp
@@ -21,27 +21,38 @@ using namespace AnimeDefendersEngine::Physics;
 
 auto PhysicsSystem::updateSystem(SceneManager& sceneManager, float fixedDeltaTime) -> void {
     using AnimeDefendersEngine::Math::Vector2f;
+
     ComponentManager& componentManager = sceneManager.getActiveScene()->getComponentManager();
     auto components = componentManager.getComponents<Collider2DComponent>();
+
     std::vector<Body*> bodies;
     bodies.reserve(components.size());
     for (auto component : components) {
         BodyDefinition bodyDef;
-        if (component) {
-            bodyDef.bodyType = BodyType::dynamicBody;
-            bodyDef.shape = std::make_unique<Rectangle>(Vector2f(2, 2));
-            bodyDef.id = component.getEntityID();
-        }
+        bodyDef.id = component.getEntityID();
+        bodyDef.shape = std::make_unique<Rectangle>(Collider2DComponent.size);
         bodyDef.transform.position = component.TransformComponent.position;
+
         Body* body = m_physicsWorld.addBody(std::move(bodyDef));
-        body->applyImpulse(component.RigidBody2DComponent.velocity);
+
+        if (component.RigidBody2DComponent == nullptr) {
+            body->setType(BodyType::staticBody);
+        } else {
+            body->setType(BodyType::dynamicBody);
+            body->applyImpulse(component.RigidBody2DComponent.velocity);
+        }
+
         bodies.push_back(body);
     }
+
     m_physicsWorld.setFixedDeltaTime(fixedDeltaTime);
     m_physicsWorld.fixedUpdate();
+
     for (auto body : bodies) {
-        components[body.getID()].TransformComponent.position = body->getPosition();
+        components.at(body->getID()).TransformComponent.position = body->getPosition();
     }
 }
 
-auto PhysicsSystem::setFixedDeltaTime(float) noexcept -> void {}
+auto PhysicsSystem::setFixedDeltaTime(float fixedDeltaTime) noexcept -> void {
+    m_physicsWorld.setFixedDeltaTime(fixedDeltaTime);
+}
