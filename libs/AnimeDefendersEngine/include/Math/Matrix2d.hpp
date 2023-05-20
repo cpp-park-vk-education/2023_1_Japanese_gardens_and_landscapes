41438d13304589740cdf79a80b7245b43e589455
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-20 17:16:48 +0300

    Removed concept from template declaration - more types of data will be  avaibale to store

--- libs/AnimeDefendersEngine/include/Math/Matrix2d.hpp
+++ libs/AnimeDefendersEngine/include/Math/Matrix2d.hpp
@@ -12,7 +12,7 @@
 namespace AnimeDefendersEngine {
     namespace Math {
 
-        template <Number T>
+        template <typename T>
         class Matrix2d {
          public:
             explicit Matrix2d(std::size_t rows = 0, std::size_t columns = 0, T val = T{})
