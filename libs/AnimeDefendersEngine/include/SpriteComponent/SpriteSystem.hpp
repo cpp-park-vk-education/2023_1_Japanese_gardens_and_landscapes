97a3b7ee1ec0f1ab827203f24837860420d8b3b3
AngelicHedgehog <serj_1313@mail.ru>
2023-05-21 01:44:00 +0300

    Declarations changes from task 15

--- libs/AnimeDefendersEngine/include/SpriteComponent/SpriteSystem.hpp
+++ libs/AnimeDefendersEngine/include/SpriteComponent/SpriteSystem.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "Camera.hpp"
+#include "DrawTextureWrapper.hpp"
 #include "ISystem.hpp"
 #include "Sprite.hpp"
+#include "TransposeMathWrapper.hpp"
 
 #include <vector>
 
@@ -11,11 +13,14 @@ namespace AnimeDefendersEngine {
 
         class SpriteSystem : public ISystem {
          public:
-            static auto isSpriteVisibleToCamera(const Sprite&, const Camera&) -> bool;
-            static auto drawSprite(const Sprite&) -> void;
+            static auto isSpriteVisibleToCamera(const Sprite*, const Camera*) -> bool;
+            static auto drawSprite(const Sprite*) -> void;
 
          private:
             SpriteSystem();
+
+            static constexpr TransposeMathWrapper transposeMathWrapper{};
+            static constexpr DrawTextureWrapper drawTextureWrapper{};
         };
 
     }  // namespace Graphics
