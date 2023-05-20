e72de713cbee06f2c3cac0e7b2fcb7def6be25eb
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-20 10:49:53 +0300

    added scene manager

--- libs/AnimeDefendersEngine/include/Systems/SystemManager.hpp
+++ libs/AnimeDefendersEngine/include/Systems/SystemManager.hpp
@@ -1,17 +1,24 @@
 #pragma once
 
-#include "ISystem.hpp"
+#include <vector>
+
+namespace AnimeDefendersEngine {
+
+    class SceneManager;
+    class ISystem;
+
+}  // namespace AnimeDefendersEngine
 
 namespace AnimeDefendersEngine {
 
     class ISystemManager {
      public:
-        virtual void updateSystems(ComponentManager&) = 0;
+        virtual void updateSystems(SceneManager&, float) = 0;
     };
 
     class SystemManager : public ISystemManager {
      public:
-        void updateSystems(ComponentManager&) override;
+        void updateSystems(SceneManager&, float) override;
 
      private:
         std::vector<ISystem*> m_systems;
