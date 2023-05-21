16aa1e930e7d1289ac1f3eb61310ba6cc71a4783
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-21 16:57:45 +0300

    fixed body.cpp

--- libs/AnimeDefendersEngine/src/Physics/Body.cpp
+++ libs/AnimeDefendersEngine/src/Physics/Body.cpp
@@ -2,52 +2,68 @@
 
 using namespace AnimeDefendersEngine::Physics;
 
-auto Body::setType(BodyType bodyType) noexcept -> void {
+auto Body::setType(BodyType bodyType) -> void {
     m_bodyType = bodyType;
 }
 
-auto Body::applyForce(const Math::Vector2f& force) noexcept -> void {
+auto Body::applyForce(const Math::Vector2f& force) -> void {
     m_force = m_force + force;
 }
 
-auto Body::applyImpulse(const Math::Vector2f& velocity) noexcept -> void {
+auto Body::applyImpulse(const Math::Vector2f& velocity) -> void {
     m_velocity = m_velocity + velocity;
 }
 
-auto Body::integrateForce(float deltaTime) noexcept -> void {
+auto Body::integrateForce(float deltaTime) -> void {
     m_velocity = m_velocity + deltaTime * m_force;
 }
 
-auto Body::integrateVelocity(float deltaTime) noexcept -> void {
+auto Body::integrateVelocity(float deltaTime) -> void {
     m_transform.position = m_transform.position + deltaTime * m_velocity;
 }
 
-auto Body::getPosition() const noexcept -> Math::Vector2f {
+auto Body::getPosition() const -> Math::Vector2f {
     return m_transform.position;
 }
 
-auto Body::isTrigger() const noexcept -> bool {
+auto Body::isTrigger() const -> bool {
     return m_isTrigger;
 }
 
-auto Body::setPosition(const Math::Vector2f& newPosition) noexcept -> void {
+auto Body::setPosition(const Math::Vector2f& newPosition) -> void {
     m_transform.position = newPosition;
 }
 
-auto Body::getVelocity() const noexcept -> Math::Vector2f {
+auto Body::getVelocity() const -> Math::Vector2f {
     return m_velocity;
 }
 
-auto Body::setVelocity(const Math::Vector2f& newVelocity) noexcept -> void {
+auto Body::getInverseMass() const -> float {
+    return m_inverseMass;
+}
+
+auto Body::setVelocity(const Math::Vector2f& newVelocity) -> void {
     m_velocity = newVelocity;
 }
 
-auto Body::getShape() const noexcept -> Shape* {
+auto Body::getType() const -> BodyType {
+    return m_bodyType;
+}
+
+auto Body::getShape() const -> Shape* {
     return m_shape.get();
 }
 
-auto Body::getShapeType() const noexcept -> ShapeType {
+auto Body::getShapeType() const -> ShapeType {
     return m_shape->getType();
+}
+
+auto Body::clearForce() -> void {
+    m_force = AnimeDefendersEngine::Math::Vector2f(0, 0);
+}
+
+auto Body::clearVelocity() -> void {
+    m_velocity = AnimeDefendersEngine::Math::Vector2f(0, 0);
 }
 
 Body::Body(BodyDefinition&& bodyDefinition)
@@ -58,4 +74,10 @@ Body::Body(BodyDefinition&& bodyDefinition)
       m_velocity(bodyDefinition.velocity),
       m_layers(bodyDefinition.layers),
       m_force(bodyDefinition.force),
-      m_isTrigger(bodyDefinition.isTrigger) {}
+      m_isTrigger(bodyDefinition.isTrigger) {
+    if (bodyDefinition.bodyType == BodyType::staticBody) {
+        m_inverseMass = 0;
+    } else {
+        m_inverseMass = 1.0f / bodyDefinition.mass;
+    }
+}
