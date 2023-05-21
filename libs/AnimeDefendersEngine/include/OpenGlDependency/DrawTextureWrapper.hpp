4d1212a04ad550b39cf40f3e2283ebc06fe0cb89
AngelicHedgehog <serj_1313@mail.ru>
2023-05-21 17:28:41 +0300

    Changed view of namespaces

--- libs/AnimeDefendersEngine/include/OpenGlDependency/DrawTextureWrapper.hpp
+++ libs/AnimeDefendersEngine/include/OpenGlDependency/DrawTextureWrapper.hpp
@@ -2,15 +2,13 @@
 
 #include "IDrawTextureWrapper.hpp"
 
-namespace AnimeDefendersEngine {
-    namespace Graphics {
+namespace AnimeDefendersEngine::Graphics {
 
-        class DrawTextureWrapper : public IDrawTextureWrapper {
-         public:
-            DrawTextureWrapper() {}
-            auto loadTexture(const Math::Matrix2d<Color>&) const -> Texture override;
-            auto drawTexture(const Texture&) const -> void override;
-        };
+    class DrawTextureWrapper : public IDrawTextureWrapper {
+     public:
+        DrawTextureWrapper() {}
+        auto loadTexture(const Math::Matrix2d<Color>&) const -> Texture override;
+        auto drawTexture(const Texture&) const -> void override;
+    };
 
-    }  // namespace Graphics
-}  // namespace AnimeDefendersEngine
+}  // namespace AnimeDefendersEngine::Graphics
--- libs/AnimeDefendersEngine/src/OpenGlDependency/DrawTextureWrapper.cpp
+++ libs/AnimeDefendersEngine/src/OpenGlDependency/DrawTextureWrapper.cpp
@@ -2,42 +2,44 @@
 
 #include <GL/glut.h>
 
-using namespace AnimeDefendersEngine::Graphics;
+namespace AnimeDefendersEngine::Graphics {
 
-auto DrawTextureWrapper::loadTexture(const Math::Matrix2d<Color>& imageData) const -> Texture {
-    Texture newTexture;
-    glGenTextures(1, &newTexture.textureId);
-    glBindTexture(GL_TEXTURE_2D, newTexture.textureId);
+    auto DrawTextureWrapper::loadTexture(const Math::Matrix2d<Color>& imageData) const -> Texture {
+        Texture newTexture;
+        glGenTextures(1, &newTexture.textureId);
+        glBindTexture(GL_TEXTURE_2D, newTexture.textureId);
 
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageData.columns(), imageData.rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, &*imageData.begin());
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageData.columns(), imageData.rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, &*imageData.begin());
 
-    glBindTexture(GL_TEXTURE_2D, 0);
+        glBindTexture(GL_TEXTURE_2D, 0);
 
-    return newTexture;
-}
+        return newTexture;
+    }
 
-auto DrawTextureWrapper::drawTexture(const Texture& texture) const -> void {
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture.textureId);
-    glPushMatrix();
+    auto DrawTextureWrapper::drawTexture(const Texture& texture) const -> void {
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, texture.textureId);
+        glPushMatrix();
 
-    glColor3f(1, 1, 1);
+        glColor3f(1, 1, 1);
 
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-    glVertexPointer(2, GL_FLOAT, 0, &*Texture::vertexCoords.begin());
-    glTexCoordPointer(2, GL_FLOAT, 0, &*Texture::textureCoords.begin());
-    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        glVertexPointer(2, GL_FLOAT, 0, &*Texture::vertexCoords.begin());
+        glTexCoordPointer(2, GL_FLOAT, 0, &*Texture::textureCoords.begin());
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 
-    glPopMatrix();
-    glDisable(GL_TEXTURE_2D);
-}
+        glPopMatrix();
+        glDisable(GL_TEXTURE_2D);
+    }
+
+}  // namespace AnimeDefendersEngine::Graphics
