5d6942e9299ca93648f2578885320c22a7386034
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-21 16:53:01 +0300

    uncommented code

--- libs/AnimeDefendersEngine/src/Physics/PhysicsSystem.cpp
+++ libs/AnimeDefendersEngine/src/Physics/PhysicsSystem.cpp
@@ -1,49 +1,49 @@
 #include "PhysicsSystem.hpp"
 #include "Body.hpp"
 #include "BodyDefinition.hpp"
+#include "Collider2DComponent.hpp"
 #include "Component.hpp"
 #include "ComponentManager.hpp"
+#include "SceneManager.hpp"
 
 #include <memory>
 
-using namespace AnimeDefendersEngine::Physics;
+using namespace AnimeDefendersEngine;
 
-auto PhysicsSystem::updateSystem(SceneManager& sceneManager, float fixedDeltaTime) -> void {
+auto Physics::PhysicsSystem::updateSystem(SceneManager& sceneManager, float fixedDeltaTime) -> void {
     using AnimeDefendersEngine::Math::Vector2f;
 
-    // Нужно сделать компоненты
+    ComponentManager& componentManager = sceneManager.getActiveScene().getComponentManager();
+    auto colliders = componentManager.getComponents<Collider2DComponent>();
 
-        // ComponentManager& componentManager = sceneManager.getActiveScene().getComponentManager();
-    // auto colliders = componentManager.getComponents<Collider2DComponent>();
+    std::vector<Physics::Body*> bodies;
+    bodies.reserve(colliders.size());
+    for (auto collider : colliders) {
+        Physics::BodyDefinition bodyDef;
+        bodyDef.id = collider.second->getEntityId();
+        bodyDef.shape = std::make_unique<Physics::Rectangle>(collider.second->size);
+        bodyDef.transform.position = collider.second->transformComponent.position;
 
-    // std::vector<Body*> bodies;
-    // bodies.reserve(colliders.size());
-    // for (auto collider : colliders) {
-    //     BodyDefinition bodyDef;
-    //     bodyDef.id = collider.getEntityID();
-    //     bodyDef.shape = std::make_unique<Rectangle>(collider.size);
-    //     bodyDef.transform.position = collider.transformComponent.position;
+        Physics::Body* body = m_physicsWorld.addBody(std::move(bodyDef));
 
-    //     Body* body = m_physicsWorld.addBody(std::move(bodyDef));
+        if (collider.second->rigidBody2DComponent == nullptr) {
+            body->setType(Physics::BodyType::staticBody);
+        } else {
+            body->setType(Physics::BodyType::dynamicBody);
+            body->applyImpulse(collider.second->rigidBody2DComponent.velocity);
+        }
 
-    //     if (collider.rigidBody2DComponent == nullptr) {
-    //         body->setType(BodyType::staticBody);
-    //     } else {
-    //         body->setType(BodyType::dynamicBody);
-    //         body->applyImpulse(collider.rigidBody2DComponent.velocity);
-    //     }
+        bodies.push_back(body);
+    }
 
-    //     bodies.push_back(body);
-    // }
+    m_physicsWorld.setFixedDeltaTime(fixedDeltaTime);
+    m_physicsWorld.fixedUpdate();
 
-    // m_physicsWorld.setFixedDeltaTime(fixedDeltaTime);
-    // m_physicsWorld.fixedUpdate();
-
-    // for (auto body : bodies) {
-    //     colliders.at(body->getID()).transformComponent.position = body->getPosition();
-    // }
+    for (auto body : bodies) {
+        colliders.at(body->getID()).transformComponent.position = body->getPosition();
+    }
 }
 
-auto PhysicsSystem::setFixedDeltaTime(float fixedDeltaTime) noexcept -> void {
+auto Physics::PhysicsSystem::setFixedDeltaTime(float fixedDeltaTime) noexcept -> void {
     m_physicsWorld.setFixedDeltaTime(fixedDeltaTime);
 }
