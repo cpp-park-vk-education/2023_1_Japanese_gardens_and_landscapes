c42a86d8f975c1af2aa43287da80427d57d44ed7
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-21 16:56:34 +0300

    fixed Body.hpp

--- libs/AnimeDefendersEngine/include/Physics/Body.hpp
+++ libs/AnimeDefendersEngine/include/Physics/Body.hpp
@@ -15,15 +15,20 @@ namespace AnimeDefendersEngine::Physics {
         virtual auto integrateForce(float deltaTime) noexcept -> void;
         virtual auto integrateVelocity(float deltaTime) noexcept -> void;
 
-        virtual auto getPosition() const noexcept -> Math::Vector2f;
-        virtual auto getVelocity() const noexcept -> Math::Vector2f;
-        virtual auto getShape() const noexcept -> Shape*;
-        virtual auto getShapeType() const noexcept -> ShapeType;
-        virtual auto isTrigger() const noexcept -> bool;
+        [[nodiscard]] virtual auto getPosition() const noexcept -> Math::Vector2f;
+        [[nodiscard]] virtual auto getVelocity() const noexcept -> Math::Vector2f;
+        [[nodiscard]] virtual auto getType() const noexcept -> BodyType;
+        [[nodiscard]] virtual auto getInverseMass() const noexcept -> float;
+        [[nodiscard]] virtual auto getShape() const noexcept -> Shape*;
+        [[nodiscard]] virtual auto getShapeType() const noexcept -> ShapeType;
+        [[nodiscard]] virtual auto isTrigger() const noexcept -> bool;
 
         virtual auto setPosition(const Math::Vector2f& newPosition) noexcept -> void;
         virtual auto setVelocity(const Math::Vector2f& newVelocity) noexcept -> void;
         virtual auto setType(BodyType type) noexcept -> void;
+
+        virtual auto clearForce() noexcept -> void;
+        virtual auto clearVelocity() noexcept -> void;
 
      private:
         size_t m_id;
@@ -31,6 +36,7 @@ namespace AnimeDefendersEngine::Physics {
         BodyType m_bodyType;
         Transform m_transform;
         Math::Vector2f m_velocity;
+        float m_inverseMass;
         size_t m_layers;
         Math::Vector2f m_force;
         bool m_isTrigger;
