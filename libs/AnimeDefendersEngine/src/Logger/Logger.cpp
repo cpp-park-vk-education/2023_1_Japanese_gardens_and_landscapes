c4512f20afed14c924f2d4b028ab622bff977022
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-22 20:52:25 +0300

    Added milliseconds

--- libs/AnimeDefendersEngine/src/Logger/Logger.cpp
+++ libs/AnimeDefendersEngine/src/Logger/Logger.cpp
@@ -3,30 +3,38 @@
 #include <chrono>
 #include <ctime>
 #include <fstream>
+#include <iomanip>
 
 namespace {
+
     constexpr auto redColor = "\033[1;31m";
     constexpr auto yellowColor = "\033[1;33m";
     constexpr auto noColor = "\033[0m";
+
 }  // namespace
 
 namespace {
+
     using AnimeDefendersEngine::Logger::LogLevel;
     [[nodiscard]] inline auto operator<(LogLevel a, LogLevel b) noexcept -> bool {
         return static_cast<char>(a) < static_cast<char>(b);
     }
 
-    struct CurrentTime {};
+    [[nodiscard]] auto currentTime() -> std::string {
+        auto timePoint = std::chrono::high_resolution_clock::now();
 
-    auto operator<<(std::ostream& stream, [[maybe_unused]] CurrentTime) -> std::ostream& {
-        auto timePoint = std::chrono::system_clock::now();
-        auto t = std::chrono::system_clock::to_time_t(timePoint);
-        std::tm* now = std::localtime(&t);
-        stream << now->tm_hour << ':' << now->tm_min << ':' << now->tm_sec << " " << now->tm_mday << '.' << (now->tm_mon + 1) << '.'
-               << (now->tm_year + 1900);
+        auto time = std::chrono::high_resolution_clock::to_time_t(timePoint);
 
-        return stream;
+        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(timePoint.time_since_epoch()) % 1000;
+
+        std::tm* now = std::localtime(&time);
+        std::stringstream stream;
+        stream << now->tm_hour << ':' << now->tm_min << ':' << now->tm_sec << ':' << std::setfill('0') << std::setw(3)
+               << milliseconds.count() << " " << now->tm_mday << '.' << (now->tm_mon + 1) << '.' << (now->tm_year + 1900);
+
+        return stream.str();
     }
+
 }  // namespace
 
 using AnimeDefendersEngine::Logger::BasicLogger;
@@ -35,14 +43,14 @@ BasicLogger::BasicLogger(std::unique_ptr<std::ostream>&& stream) : m_stream{std:
 
 auto BasicLogger::printMessage(const std::string& message) const -> const BasicLogger& {
     if (m_logLevel >= LogLevel::ErrorsWarningsAndMessages) {
-        *m_stream << "Message [" << CurrentTime{} << "] " << message << '\n';
+        *m_stream << "Message [" << currentTime() << "] " << message << '\n';
     }
     return *this;
 }
 
 auto BasicLogger::printWarning(const std::string& message) const -> const BasicLogger& {
     if (m_logLevel >= LogLevel::ErrorsAndWarnings) {
-        *m_stream << yellowColor << "Warning [" << CurrentTime{} << "] " << message << noColor << '\n';
+        *m_stream << yellowColor << "Warning [" << currentTime() << "] " << message << noColor << '\n';
     }
 
     return *this;
@@ -50,7 +58,7 @@ auto BasicLogger::printWarning(const std::string& message) const -> const BasicL
 
 auto BasicLogger::printError(const std::string& message) const -> const BasicLogger& {
     if (m_logLevel >= LogLevel::OnlyErrors) {
-        *m_stream << redColor << "Error [" << CurrentTime{} << "] " << message << noColor << '\n';
+        *m_stream << redColor << "Error   [" << currentTime() << "] " << message << noColor << '\n';
     }
 
     return *this;
