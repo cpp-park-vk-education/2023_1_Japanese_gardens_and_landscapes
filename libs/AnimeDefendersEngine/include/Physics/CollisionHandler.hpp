08c499bc58af1aa6ec9507320e2df74bcd612327
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-23 11:36:34 +0300

    renamed argument

--- libs/AnimeDefendersEngine/include/Physics/CollisionHandler.hpp
+++ libs/AnimeDefendersEngine/include/Physics/CollisionHandler.hpp
@@ -17,7 +17,7 @@ namespace AnimeDefendersEngine::Physics {
     class CollisionHandler final : public ICollisionHandler {
      public:
         [[nodiscard]] virtual auto broadPhase(const std::vector<Body*>& bodies) const -> std::unordered_set<Manifold> override;
-        virtual auto narrowPhase(std::unordered_set<Manifold>& bodies) const -> void override;
+        virtual auto narrowPhase(std::unordered_set<Manifold>& contacts) const -> void override;
         [[nodiscard]] virtual auto hasCollision(Body* bodyA, Body* bodyB) const -> bool override;
         virtual auto specifyCollision(Manifold& contact) const -> void override;
         virtual auto resolveCollision(Manifold& contact) const -> void override;
