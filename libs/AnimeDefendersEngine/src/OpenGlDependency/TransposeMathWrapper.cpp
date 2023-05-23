90a3031c04228b625a16ede0fd540b7eb240b1f5
Sergey Dmitrievich <serj_1313@mail.ru>
2023-05-24 01:48:49 +0300

    Changed pointer to link

--- libs/AnimeDefendersEngine/src/OpenGlDependency/TransposeMathWrapper.cpp
+++ libs/AnimeDefendersEngine/src/OpenGlDependency/TransposeMathWrapper.cpp
@@ -4,11 +4,11 @@
 
 namespace AnimeDefendersEngine::Graphics {
 
-    auto TransposeMathWrapper::applyTransposeMatrix(const Math::Transpose* transpose) const noexcept -> void {
+    auto TransposeMathWrapper::applyTransposeMatrix(const Math::Transpose& transpose) const noexcept -> void {
         glPushMatrix();
-        glRotated(transpose->vecticalViewAngle, 1, 0, 0);
-        glRotated(transpose->horisontalViewAngle, 0, 1, 0);
-        glTranslated(transpose->posCoords.x, transpose->posCoords.y, transpose->posCoords.z);
+        glRotated(transpose.vecticalViewAngle, 1, 0, 0);
+        glRotated(transpose.horisontalViewAngle, 0, 1, 0);
+        glTranslated(transpose.posCoords.x, transpose.posCoords.y, transpose.posCoords.z);
     }
 
     auto TransposeMathWrapper::dropLastMatrix() const noexcept -> void {
