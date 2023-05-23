58b437e8327845851d5d5be3e1fbaf877648f0e8
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-24 00:26:58 +0300

    Minor fixes

--- libs/AnimeDefendersEngine/include/AnimeFramework/Event.hpp
+++ libs/AnimeDefendersEngine/include/AnimeFramework/Event.hpp
@@ -16,16 +16,16 @@ namespace AnimeDefendersEngine {
     };
 
     class Event {
+     public:
+        Event() = default;
+        Event(const std::string& name, EventType type, const std::string& add) : m_name(name), m_eventType(type), m_additional(add) {}
+        auto getType() const -> EventType { return m_eventType; }
+        auto getName() const -> const std::string& { return m_name; }
+
      private:
         std::string m_name{};
         std::string m_additional{};
         EventType m_eventType{};
-
-     public:
-        Event() = default;
-        Event(std::string name, EventType type, std::string add = "") : m_name(name), m_eventType(type), m_additional(add) {}
-        auto getType() -> EventType { return m_eventType; }
-        auto getName() -> std::string { return m_name; }
     };
 
 }  // namespace AnimeDefendersEngine
