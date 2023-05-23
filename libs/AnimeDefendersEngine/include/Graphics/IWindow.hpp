c9eef4217cdd2ed3e7d6c39b84c5e4b746d03c52
Sergey Dmitrievich <serj_1313@mail.ru>
2023-05-24 01:41:17 +0300

    Added virtual destructor

--- libs/AnimeDefendersEngine/include/Graphics/IWindow.hpp
+++ libs/AnimeDefendersEngine/include/Graphics/IWindow.hpp
@@ -8,6 +8,8 @@ namespace AnimeDefendersEngine::Graphics {
      public:
         virtual auto updateFrame() const noexcept -> void = 0;
 
+        virtual ~IWindow() = default;
+
         virtual auto setWindowMouseHandler(std::function<void(int, int, int, int)>) const noexcept -> void = 0;
         virtual auto setWindowMotionHandler(std::function<void(int, int)>) const noexcept -> void = 0;
         virtual auto setWindowKeyboardHandler(std::function<void(unsigned char, int, int)>) const noexcept -> void = 0;
