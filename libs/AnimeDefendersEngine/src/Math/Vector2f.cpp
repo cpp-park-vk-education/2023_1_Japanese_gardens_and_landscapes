60383b55ae764efa83a7aef1f3385619867abc54
MiraiRatchet <mirairatchetx@gmail.com>
2023-05-23 20:28:14 +0300

    Minor fixes

--- libs/AnimeDefendersEngine/src/Math/Vector2f.cpp
+++ libs/AnimeDefendersEngine/src/Math/Vector2f.cpp
@@ -1,40 +1,51 @@
 #include "Vector2f.hpp"
 
-auto AnimeDefendersEngine::Math::inverseSquareRoot(float number) -> float {
-    const float threeHalves = 1.5f;
-    float y = number;
-    long i = *(long*)&y;
-    i = 0x5f3759df - (i >> 1);
-    y = *(float*)&i;
-    y = y * (threeHalves - (number * 0.5f * y * y));
-    return y;
-}
+#include <cstdint>
+#include <cstring>
 
-auto AnimeDefendersEngine::Math::Vector2f::norm() const -> float {
-    return 1 / inverseSquareRoot((*this) * (*this));
-}
+namespace {
+    constexpr int invSquareRootConstant = 0x5f3759df;
 
-std::ostream& AnimeDefendersEngine::Math::operator<<(std::ostream& os, const Vector2f& vec) {
-    os << "(" << vec.x << ", " << vec.y << ")";
-    return os;
-}
+    auto inverseSquareRoot(float number) -> float {
+        float xhalf = 0.5f * number;
+        std::uint32_t i;
+        std::memcpy(&i, &number, sizeof(i));
+        i = invSquareRootConstant - (i >> 1);
+        std::memcpy(&number, &i, sizeof(number));
+        number *= (1.5f - xhalf * number * number);
+        return number;
+    }
+}  // namespace
 
-auto AnimeDefendersEngine::Math::operator+(const Vector2f& vec1, const Vector2f& vec2) -> Vector2f {
-    return Vector2f{vec1.x + vec2.x, vec1.y + vec2.y};
-}
+namespace AnimeDefendersEngine {
 
-auto AnimeDefendersEngine::Math::operator-(const Vector2f& vec1, const Vector2f& vec2) -> Vector2f {
-    return Vector2f{vec1.x - vec2.x, vec1.y - vec2.y};
-}
+    auto Math::Vector2f::norm() const -> float {
+        return 1 / inverseSquareRoot((*this) * (*this));
+    }
 
-auto AnimeDefendersEngine::Math::operator*(const Vector2f& vec1, const Vector2f& vec2) -> float {
-    return vec1.x * vec2.x + vec1.y * vec2.y;
-}
+    std::ostream& Math::operator<<(std::ostream& os, const Vector2f& vec) {
+        os << "(" << vec.x << ", " << vec.y << ")";
+        return os;
+    }
 
-auto AnimeDefendersEngine::Math::operator*(const float& c1, const Vector2f& vec1) -> Vector2f {
-    return Vector2f{c1 * vec1.x, c1 * vec1.y};
-}
+    auto Math::operator+(const Vector2f& vec1, const Vector2f& vec2) -> Vector2f {
+        return Vector2f{vec1.x + vec2.x, vec1.y + vec2.y};
+    }
 
-auto AnimeDefendersEngine::Math::operator*(const Vector2f& vec1, const float& c1) -> Vector2f {
-    return Vector2f{c1 * vec1.x, c1 * vec1.y};
-}
+    auto Math::operator-(const Vector2f& vec1, const Vector2f& vec2) -> Vector2f {
+        return Vector2f{vec1.x - vec2.x, vec1.y - vec2.y};
+    }
+
+    auto Math::operator*(const Vector2f& vec1, const Vector2f& vec2) -> float {
+        return vec1.x * vec2.x + vec1.y * vec2.y;
+    }
+
+    auto Math::operator*(const float& c1, const Vector2f& vec1) -> Vector2f {
+        return Vector2f{c1 * vec1.x, c1 * vec1.y};
+    }
+
+    auto Math::operator*(const Vector2f& vec1, const float& c1) -> Vector2f {
+        return Vector2f{c1 * vec1.x, c1 * vec1.y};
+    }
+
+}  // namespace AnimeDefendersEngine
