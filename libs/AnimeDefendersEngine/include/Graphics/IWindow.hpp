76c0c677d95f7b8b3ddf7d1f2888a5cdc33254a4
Sergey Dmitrievich <serj_1313@mail.ru>
2023-05-24 02:10:17 +0300

    Added names to args of handlers

--- libs/AnimeDefendersEngine/include/Graphics/IWindow.hpp
+++ libs/AnimeDefendersEngine/include/Graphics/IWindow.hpp
@@ -4,9 +4,9 @@
 
 namespace AnimeDefendersEngine::Graphics {
 
-    using mouseClickHandler = std::function<void(int, int, int, int)>;
-    using mouseMotionHandler = std::function<void(int, int)>;
-    using keyPressHandler = std::function<void(unsigned char, int, int)>;
+    using mouseClickHandler = std::function<void(int buttonNum, int buttonState, int xMouse, int yMouse)>;
+    using mouseMotionHandler = std::function<void(int xMouse, int yMouse)>;
+    using keyPressHandler = std::function<void(unsigned char keyChar, int xMouse, int yMouse)>;
 
     class IWindow {
      public:
