238586a6ea12e76358b62589da6ae4a759ee47d3
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-23 17:20:16 +0300

    Resolved conflicts

--- libs/AnimeDefendersEngine/src/Physics/BodyDefinition.cpp
+++ libs/AnimeDefendersEngine/src/Physics/BodyDefinition.cpp
@@ -1,24 +1,16 @@
 #include "BodyDefinition.hpp"
 
-using namespace AnimeDefendersEngine::Physics;
+namespace AnimeDefendersEngine::Physics {
 
-BodyDefinition::BodyDefinition(size_t id, std::unique_ptr<Shape>&& shapeUPtr, BodyType bodyType, Transform transform,
-                               Math::Vector2<float> velocity, size_t layers, Math::Vector2<float> force, bool isTrigger)
-    : id(id),
-      shapeUPtr(std::move(shapeUPtr)),
-      bodyType(bodyType),
-      transform(transform),
-      velocity(velocity),
-      layers(layers),
-      force(force),
-      isTrigger(isTrigger){};
+    BodyDefinition::BodyDefinition(size_t id, std::unique_ptr<Shape>&& shapeUPtr, BodyType bodyType, Transform transform,
+                                   Math::Vector2<float> velocity, size_t layers, Math::Vector2<float> force, bool isTrigger)
+        : id(id),
+          shapeUPtr(std::move(shapeUPtr)),
+          bodyType(bodyType),
+          transform(transform),
+          velocity(velocity),
+          layers(layers),
+          force(force),
+          isTrigger(isTrigger){};
 
-BodyDefinition::BodyDefinition(BodyDefinition&& other)
-    : id(other.id),
-      shapeUPtr(std::move(other.shapeUPtr)),
-      bodyType(other.bodyType),
-      transform(other.transform),
-      velocity(other.velocity),
-      layers(other.layers),
-      force(other.force),
-      isTrigger(other.isTrigger) {}
+} // namespace AnimeDefendersEngine::Physics
