595abba22fa43cc1f5c1e7311773bd4465808ea8
BorisKaryshev <boris.karyshev@gmail.com>
2023-05-24 12:28:58 +0300

    Added image loading

--- libs/AnimeDefendersEngine/include/FileSystem/FileSystem.hpp
+++ libs/AnimeDefendersEngine/include/FileSystem/FileSystem.hpp
@@ -1,13 +1,56 @@
 #pragma once
 
+#include "Color.hpp"
+#include "Matrix2d.hpp"
+#include "TableReader.hpp"
+
+#include <concepts>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+#ifndef SOURCE_DIR_PATH
+#define SOURCE_DIR_PATH ""
+#endif
+
+inline constexpr auto absolutePath(const std::convertible_to<std::string> auto& x) -> std::string {
+    return std::string{SOURCE_DIR_PATH} + x;
+}
+
 namespace AnimeDefendersEngine {
+    using Graphics::Color;
+
     namespace FileSystem {
 
         struct Audio {};
 
         struct AudioTrack {};
 
-        struct Image {};
+        using Image = Math::Matrix2d<Color>;
+
+        enum class FileType {
+            imagePNG,
+            text
+        };
+
+        class FileSystem {
+         public:
+            explicit FileSystem(const std::filesystem::path& pathToRoot);
+
+            auto loadItem(const std::string& name) -> void;
+            auto getImage(const std::string& name) -> const Image&;
+
+            auto deleteItem(FileType type, const std::string& name) -> void;
+
+         private:
+            std::unordered_map<std::string, Image> m_images;
+            std::unordered_map<std::string, Audio> m_audios;
+            std::unordered_map<std::string, AudioTrack> m_audioTracks;
+
+            std::unordered_map<std::string, std::filesystem::path> m_paths;
+        };
 
     }  // namespace FileSystem
+
 }  // namespace AnimeDefendersEngine
--- libs/AnimeDefendersEngine/src/FileSystem/FileSystem.cpp
+++ libs/AnimeDefendersEngine/src/FileSystem/FileSystem.cpp
@@ -1,1 +1,73 @@
 #include "FileSystem.hpp"
+
+#include <iostream>
+#include "stb_image.h"
+
+namespace AnimeDefendersEngine::FileSystem {
+
+    namespace {
+
+        auto returnPixel(unsigned char* data, std::size_t i, std::size_t j, std::size_t x, std::size_t channelCount) -> Color {
+            Color pixel;
+            const unsigned char* pixelOffset = data + (i + x * j) * channelCount;
+            pixel.red = pixelOffset[0];
+            pixel.green = pixelOffset[1];
+            pixel.blue = pixelOffset[2];
+            pixel.alpha = pixelOffset[3];
+
+            return pixel;
+        }
+
+    }  // namespace
+
+    FileSystem::FileSystem(const std::filesystem::path& pathToRoot) {
+        TableReader reader{std::make_unique<std::ifstream>(pathToRoot), '|'};
+        while (!reader.endOfTable()) {
+            m_paths[reader["Name"]] = absolutePath(reader["FilePath"]);
+            
+            reader.readNewRow();
+        }
+
+        for (const auto& [name, path] : m_paths) {
+            std::cout << "name = '" << name << "' path = '" << path.c_str() << "'\n";
+        }
+    }
+
+    auto FileSystem::loadItem(const std::string& name) -> void {
+        if (m_paths.at(name).extension().string() == ".png") {
+            int x{}, y{}, comps{};
+            unsigned char* data = stbi_load(m_paths.at(name).c_str(), &x, &y, &comps, 0);
+
+            std::vector<Color> pixels;
+            pixels.reserve(x * y);
+
+            for (int i = 0; i < x; ++i) {
+                for (int j = 0; j < y; ++j) {
+                    pixels.push_back(returnPixel(data, i, j, x, comps));
+                }
+            }
+
+            m_images[name] = Image{static_cast<std::size_t>(x), static_cast<std::size_t>(y), pixels};
+        }
+    }
+
+    auto FileSystem::getImage(const std::string& name) -> const Image& {
+        if (!m_images.contains(name)) {
+            loadItem(name);
+        }
+
+        return m_images[name];
+    }
+
+    auto FileSystem::deleteItem(FileType type, const std::string& name) -> void {
+        switch (type) {
+            case FileType::imagePNG:
+                m_images.erase(name);
+                break;
+
+            default:
+                break;
+        }
+    };
+
+}  // namespace AnimeDefendersEngine::FileSystem
