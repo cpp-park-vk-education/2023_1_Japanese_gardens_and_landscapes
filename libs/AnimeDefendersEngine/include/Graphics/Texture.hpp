e2c2e8fa14f21838e43acef73e773db86f70e52c
Sergey Dmitrievich <serj_1313@mail.ru>
2023-05-24 03:01:41 +0300

    Written doc to Texture

--- libs/AnimeDefendersEngine/include/Graphics/Texture.hpp
+++ libs/AnimeDefendersEngine/include/Graphics/Texture.hpp
@@ -6,7 +6,9 @@ namespace AnimeDefendersEngine::Graphics {
 
     struct Texture {
         unsigned int textureId{};
+        /// @brief Container of x/y coords for set fullscreen draw option
         static constexpr std::array<float, 8> vertexCoords{-1, -1, 1, -1, 1, 1, -1, 1};
+        /// @brief Container of x/y coords for setting part of texture to draw
         static constexpr std::array<float, 8> textureCoords{0, 0, 1, 0, 1, 1, 0, 1};
     };
 
