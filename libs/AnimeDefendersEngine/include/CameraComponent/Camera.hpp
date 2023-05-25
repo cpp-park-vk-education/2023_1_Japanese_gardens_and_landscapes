08f8d2a4976fa33b4e180791d0ab4d7b230535b6
Sergey Dmitrievich <serj_1313@mail.ru>
2023-05-25 23:44:32 +0300

    Fixed mistake

--- libs/AnimeDefendersEngine/include/CameraComponent/Camera.hpp
+++ libs/AnimeDefendersEngine/include/CameraComponent/Camera.hpp
@@ -46,7 +46,7 @@ namespace AnimeDefendersEngine::Graphics {
         transposeChangingFunctor m_extraMotionFunctor;
         bool m_isActive{false};
 
-        static constexpr ITransposeMathWrapper& m_transposeMathWrapper{};
+        static constexpr TransposeMathWrapper& m_transposeMathWrapper{};
     };
 
 }  // namespace AnimeDefendersEngine::Graphics
