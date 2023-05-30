7d7b9c7ccaa0141da788b03acf4eb5489e7c9fe5
GoshaSerbin <gosha_serbin00@mail.ru>
2023-05-30 12:30:30 +0300

    added const & to component

--- libs/AnimeDefendersEngine/src/OnFixedUpdateComponent/OnFixedUpdateSystem.cpp
+++ libs/AnimeDefendersEngine/src/OnFixedUpdateComponent/OnFixedUpdateSystem.cpp
@@ -6,7 +6,7 @@ namespace AnimeDefendersEngine {
 
     auto OnFixedUpdateSystem::updateSystem(SceneManager& sceneManager) -> void {
         auto components = sceneManager.getActiveScene().getComponentManager().getComponents<OnFixedUpdateComponent>();
-        for (auto component : components) {
+        for (const auto& component : components) {
             auto* onUpdateComponent = static_cast<OnFixedUpdateComponent*>(component.second);
             onUpdateComponent->onFixedUpdate();
         }
