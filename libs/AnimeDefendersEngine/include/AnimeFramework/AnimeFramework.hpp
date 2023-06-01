244bc24af65c326a0edfc66da4ed051485d7b1c7
BorisKaryshev <boris.karyshev@gmail.com>
2023-06-01 11:43:30 +0300

    init

--- libs/AnimeDefendersEngine/include/AnimeFramework/AnimeFramework.hpp
+++ libs/AnimeDefendersEngine/include/AnimeFramework/AnimeFramework.hpp
@@ -1,23 +1,55 @@
 #pragma once
 
 #include "ComponentManager.hpp"
+#include "Entity.hpp"
+#include "EventManager.hpp"
 #include "GameLoop.hpp"
+#include "IEntityCreator.hpp"
+#include "Renderer.hpp"
+#include "SceneLoader.hpp"
 #include "SceneManager.hpp"
+#include "SystemManager.hpp"
+#include "Window.hpp"
+
+#include <fstream>
+#include <memory>
 
 namespace AnimeDefendersEngine {
 
+    /**
+     * @brief Dummy required by AnimeFramework
+     *  Must be defined by game developer
+     */
+    class BaseCreator : public IEntityCreator {
+     public:
+        auto create(const std::vector<std::string>& arguments, Scene& scene) -> std::shared_ptr<Entity> override {
+            return std::make_shared<Entity>("Hi", scene);
+        };
+    };
+
+    /**
+     * @brief Should be redefined by game developer
+     */
     class AnimeFramework {
      public:
-        void run();
-        /* {
-                m_sceneManager = initializer.makeSceneManager(file);
-                m_componentManager = initializer.makeSceneManager(file);
-                GameLoop.run();
-        } */
+        explicit AnimeFramework()
+            : m_sceneManager{},
+              m_gameLoop{std::make_unique<SystemManager>(),
+                         std::make_unique<Graphics::Renderer>(std::make_unique<Graphics::Window>(1280, 1280, "Gaaaaaay")),
+                         std::make_unique<EventManager>(), m_sceneManager} {}
+
+        auto run() -> void {
+            auto readStream = std::make_unique<std::ifstream>("SamplePath");
+            auto entityCreator = std::make_unique<BaseCreator>();
+
+            SceneLoader loadScenes{std::move(readStream), std::move(entityCreator), m_sceneManager};
+            m_sceneManager.setActiveScene(2);
+
+            m_gameLoop.run();
+        }
 
      private:
         SceneManager m_sceneManager;
-        ComponentManager m_globalComponentsManager;
         GameLoop m_gameLoop;
     };
 }  // namespace AnimeDefendersEngine
